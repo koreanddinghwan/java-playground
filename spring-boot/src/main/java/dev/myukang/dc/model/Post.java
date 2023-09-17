f25c8b3d847595719b0344a125c00ffbfc75a187
koreanddinghwan <myunghwan0421@gmail.com>
2023-09-17 18:29:22 +0900

    backup

--- spring-boot/src/main/java/dev/myukang/dc/model/Post.java
+++ spring-boot/src/main/java/dev/myukang/dc/model/Post.java
@@ -1,12 +1,12 @@
-package dev.myukang.dc.model;
+// package dev.myukang.dc.model;
 
-import jakarta.persistence.Entity;
+// import jakarta.persistence.Entity;
 
-@Entity
-public class Post {
-	@Id
-	@GeneratedValue
-	private Integer id;
-	private String title;
-	private String body;
-}
+// @Entity
+// public class Post {
+// 	@Id
+// 	@GeneratedValue
+// 	private Integer id;
+// 	private String title;
+// 	private String body;
+// }

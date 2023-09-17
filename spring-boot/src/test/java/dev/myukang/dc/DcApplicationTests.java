eea147563961b1d4e5fd62160a48dab1361f7b81
koreanddinghwan <myunghwan0421@gmail.com>
2023-09-17 15:49:28 +0900

    [Chore]: move spring-boot

--- spring-boot/src/test/java/dev/myukang/dc/DcApplicationTests.java
+++ spring-boot/src/test/java/dev/myukang/dc/DcApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package dev.myukang.dc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class DcApplicationTests {
 
 	@Test
 	void contextLoads() {
